a3b420bec25d764342a51a0ef91c17b6a98fa392
Rakesh-dev07 <ry57520s@gmail.com>
2025-10-28 22:07:43 +0530

    first commit

---
+++ client/src/pages/student/home/index.jsx
@@ -0,0 +1,126 @@
+import { courseCategories } from "@/config";
+// import banner from "../../../../public/banner-img.png";
+import { Button } from "@/components/ui/button";
+import { useContext, useEffect } from "react";
+import { StudentContext } from "@/context/student-context";
+import {
+  checkCoursePurchaseInfoService,
+  fetchStudentViewCourseListService,
+} from "@/services";
+import { AuthContext } from "@/context/auth-context";
+import { useNavigate } from "react-router-dom";
+
+function StudentHomePage() {
+  const { studentViewCoursesList, setStudentViewCoursesList } =
+    useContext(StudentContext);
+  const { auth } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  function handleNavigateToCoursesPage(getCurrentId) {
+    console.log(getCurrentId);
+    sessionStorage.removeItem("filters");
+    const currentFilter = {
+      category: [getCurrentId],
+    };
+
+    sessionStorage.setItem("filters", JSON.stringify(currentFilter));
+
+    navigate("/courses");
+  }
+
+  async function fetchAllStudentViewCourses() {
+    const response = await fetchStudentViewCourseListService();
+    if (response?.success) setStudentViewCoursesList(response?.data);
+  }
+
+  async function handleCourseNavigate(getCurrentCourseId) {
+    const response = await checkCoursePurchaseInfoService(
+      getCurrentCourseId,
+      auth?.user?._id
+    );
+
+    if (response?.success) {
+      if (response?.data) {
+        navigate(`/course-progress/${getCurrentCourseId}`);
+      } else {
+        navigate(`/course/details/${getCurrentCourseId}`);
+      }
+    }
+  }
+
+  useEffect(() => {
+    fetchAllStudentViewCourses();
+  }, []);
+
+  return (
+    <div className="min-h-screen bg-white">
+      <section className="flex flex-col lg:flex-row items-center py-8 px-4 lg:px-8">
+        <div className="lg:w-1/2 lg:pr-10 lg:pl-10">
+          <h1 className="text-5xl font-bold mb-4">Start learning today.<br/> Shape your tomorrow.</h1>
+          <p className="text-xl">
+          Learning that gets you ahead  skills for today,<br/>success for tomorrow.
+          </p>
+          <p className="text-xl">
+          Flexible learning for busy lives Learn at your pace.
+          </p>
+        </div>
+        <div className="flex items-center justify-center lg:w-[80vh]">
+          <img
+            src="./banner-img.png"
+            // width={350}
+            // height={150}
+            className="rounded-lg shadow-lg w-full"
+          />
+        </div>
+      </section>
+      <section className="py-8 px-4 lg:px-8 bg-gray-100">
+        <h2 className="text-2xl font-bold mb-6">Course Categories</h2>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+          {courseCategories.map((categoryItem) => (
+            <Button
+              className="justify-start"
+              variant="outline"
+              key={categoryItem.id}
+              onClick={() => handleNavigateToCoursesPage(categoryItem.id)}
+            >
+              {categoryItem.label}
+            </Button>
+          ))}
+        </div>
+      </section>
+      <section className="py-12 px-4 lg:px-8">
+        <h2 className="text-2xl font-bold mb-6">Featured Courses</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+          {studentViewCoursesList && studentViewCoursesList.length > 0 ? (
+            studentViewCoursesList.map((courseItem) => (
+              <div
+                onClick={() => handleCourseNavigate(courseItem?._id)}
+                className="border rounded-lg overflow-hidden shadow cursor-pointer"
+              >
+                <img
+                  src={courseItem?.image}
+                  width={300}
+                  height={150}
+                  className="w-full h-40 object-cover"
+                />
+                <div className="p-4">
+                  <h3 className="font-bold mb-2">{courseItem?.title}</h3>
+                  <p className="text-sm text-gray-700 mb-2">
+                    {courseItem?.instructorName}
+                  </p>
+                  <p className="font-bold text-[16px]">
+                    ${courseItem?.pricing}
+                  </p>
+                </div>
+              </div>
+            ))
+          ) : (
+            <h1>No Courses Found</h1>
+          )}
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default StudentHomePage;
